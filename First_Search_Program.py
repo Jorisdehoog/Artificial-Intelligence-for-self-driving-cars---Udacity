dcc95a201aabf2b2ed3053eccfb8e67742ca3ae7
Joris <dehoogjoris@gmail.com>
2017-05-10 23:04:30 +0200

    A lot has been done, but we're not there yet. I'm struggling with indexing the current_pos matrix, with the variable "steps"
    TODO: fix dat shit

--- First_Search_Program.py
+++ First_Search_Program.py
@@ -34,22 +34,64 @@ delta_name = ['^', '<', 'v', '>']
 def search(grid,init,goal,cost):
     path = 1        # remove once finished
     # create a list of the current position. First element is the g-number, the next two are the coordinates
-    current_pos = [0]
-    current_pos[1:2] = init
+    current_pos = [[0, init[0], init[1]]]   # initialize the G-value
+    # current_pos[1:2] = init
 
     # initiate loop (while-loop?)
     # check if current position is the final result
+    goal_reached = 0    # identifier of success
+    steps = 0           # number of steps needed (current_pos[0])
+    position = init     # position (current_pos[1:2])
+
+    # print grid[position[0]][position[1]]
+    print [1, current_pos[steps][1], current_pos[steps][2]]
+
+    print len(current_pos)
+
+    # grid2 = list(grid)
+    # grid2.pop(0)
+    #
+    # print grid2
+
+    while goal_reached == 0:
+        # look for other positions.
+        grid[current_pos[steps][1]][current_pos[steps][2]] = 1
+        last_length = len(current_pos)      # so we dont change the length of the inner loop during operation
+
+        # if [current_pos[steps][1], current_pos[steps][2]] == goal:
+        #     goal_reached = 1
+        # else:
+        #     current_pos.pop(steps)
+
+        for i in range(len(delta)):
+            for ii in range(last_length):
+                newstep = [a + b for a, b in zip([current_pos[ii][1], current_pos[ii][2]], delta[i])]
+                print newstep
+                if sum(n < 0 for n in newstep) > 0 or grid[newstep[0]][newstep[1]] == 1:
+                    # do nothing
+                    print ""
+                else:
+                    current_pos.append([steps, newstep[0], newstep[1]])
+                    grid[newstep[0]][newstep[1]] = 1
+        steps += 1
+
+
+
+
+            # print grid
+
+
 
     # check neighbors (expand the one with the smallest g-value)
     # some don't need to be considered anymore as these are already checked... (create a new grid, same size as the maze
-    # to keep track of the checked positions!)
+    # to keep track of the checked positions! --> NO, just put ones where there were zeros!)
 
     # remove the nodes that have no un-checked neighbors
 
     # recurse the loop
 
 
-    print current_pos
+    # print current_pos
 
 
     return path
