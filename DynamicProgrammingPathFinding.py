423109dd42076c7e3b18afd2068f58cb224f5d60
Joris <dehoogjoris@gmail.com>
2017-05-21 13:34:29 +0200

    Works for solvable solutions, not for ones with a clear barrier.

--- DynamicProgrammingPathFinding.py
+++ DynamicProgrammingPathFinding.py
@@ -28,8 +28,55 @@ delta_name = ['^', '<', 'v', '>']
 def compute_value(grid, goal, cost):
     # ----------------------------------------
     # insert code below
+    # use the movement functions
     # ----------------------------------------
+    # set the obtacles to 99
+    value = grid
+    init = [len(grid)-1, len(grid[0])-1]
+
+
+    for i in range(len(grid)):
+        for ii in range(len(grid[0])):
+            if value[i][ii] == 1:
+                value[i][ii] = 99
+
+    x = init[0]
+    y = init[1]
+    v = 0
+
+    open = [[v, x, y]]
+    count = 0
+
+    complete = False
+    while complete is False:
+
+        open.sort()
+        open.reverse()
+        next = open.pop()
+        x = next[1]
+        y = next[2]
+        v = next[0]
+
+        for i in range(len(delta)):
+            x2 = x + delta[i][0]
+            y2 = y + delta[i][1]
+            if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                if value[x2][y2] == 0:
+                    v2 = value[x][y] + 1
+                    value[x2][y2] = v2
+                    open.append([v2, x2, y2])
+
+        if len(open) == 0:
+            value[init[0]][init[1]] = 0
+            complete = True
+
+
+
 
     # make sure your function returns a grid of values as
     # demonstrated in the previous video.
     return value
+
+pp = compute_value(grid, goal, cost)
+for row in pp:
+    print row
